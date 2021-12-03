738d49b9bd482fdb46bcff7413d46a9bd8a10833
Narasimha1997 <yogaimpression@gmail.com>
2021-12-03 19:33:19 +0530

    Added process stack allocator

--- r3_kernel/src/system/utils.rs
+++ r3_kernel/src/system/utils.rs
@@ -5,10 +5,12 @@ use alloc::vec::Vec;
 use crate::mm::stack::STACK_SIZE;
 use crate::system::filesystem::FileDescriptor;
 
+use core::mem;
+
 use crate::mm::{
     paging::KernelVirtualMemoryManager, paging::Page, paging::PageEntryFlags,
-    paging::VirtualMemoryManager, phy::Frame, Alignment, MemorySizes, PhysicalAddress,
-    VirtualAddress,
+    paging::VirtualMemoryManager, phy::Frame, phy::PhysicalMemoryManager, Alignment, Alignment,
+    MemorySizes, PhysicalAddress, VirtualAddress,
 };
 
 /// Area in which user code will be allocated
@@ -20,22 +22,29 @@ pub const USER_STACK_ADDRESS: u64 = 0x200000000;
 // | --- .text, .bss, .data ---- | ---- heap memory ----- | ---- stacks for each thread ------|
 // argv and envs |
 
-/*
-    Allocation details:
-    1. .text, .bss and .data: Will be allocated starting from 0x100000000
-        and ends at a 2MiB aligned address.
-    2. heap allocation starts from that region and has a max size
-    3. Stack allocation starts from where heap ends and has a max size as well.
+/// Start of the user virtual address space
+pub const USER_VIRT_START: u64 = 0;
 
-*/
+/// End of the user virtual address space
+pub const USER_VIRT_END: u64 = 0x800000000000;
+
+/// Stack area size, stacks are allocated as follows:
+/// Each stack will be of size 2MiB follwed by a 2MiB virtual gap
+/// So each allocation will cost 2MiB physically and 4MiB virtually
+pub const PROCESS_STACKS_SIZE: u64 = 16 * MemorySizes::OneGiB as u64;
+
+pub const THREAD_STACK_SIZE: u64 = 2 * MemorySizes::OneMib as u64;
 
 #[derive(Debug, Clone)]
 pub struct ProcessData {
     /// current allocated page allocated stack
-    pub stack_space_end: usize,
+    pub stack_space_start: VirtualAddress,
     /// contains 2MiB holes in stack space, will be reallocated from this hole
     /// upon request if any.
-    pub free_holes: Vec<VirtualAddress>,
+    pub free_stack_holes: Vec<u64>,
+    /// current allocated stack end, will be incremented by 2 for each
+    /// allocation.
+    pub n_stacks: u64,
     /// start address from where process heap is allocated
     pub heap_start: VirtualAddress,
     /// current heap size
@@ -43,7 +52,81 @@ pub struct ProcessData {
     /// list of open file descriptors for the process
     pub file_descriptors: Vec<FileDescriptor>,
     /// proc code entrypoint
-    pub code_start: VirtualAddress,
+    pub code_entry: VirtualAddress,
+}
+
+pub struct ProcessStackManager;
+
+impl ProcessStackManager {
+    #[inline]
+    fn zero(addr: VirtualAddress) {
+        // TODO: find the fastest way to do this.
+        let slice: &mut [u128; THREAD_STACK_SIZE as usize / mem::size_of::<u128>()] =
+            unsafe { &mut *addr.get_mut_ptr() };
+
+        for element in slice.iter_mut() {
+            *element = 0;
+        }
+    }
+
+    #[inline]
+    pub fn allocate_stack(
+        proc_data: &mut ProcessData,
+        vmm: &mut VirtualMemoryManager,
+    ) -> Option<VirtualAddress> {
+        // is there a free stack in the pool?
+        if proc_data.free_stack_holes.len() > 0 {
+            let stk_index = proc_data.free_stack_holes.pop().unwrap();
+            // return it's address:
+            let vaddr = VirtualAddress::from_u64(
+                proc_data.stack_space_start.as_u64() + (stk_index * 4 * MemorySizes::OneMib as u64),
+            );
+
+            Self::zero(vaddr);
+            return Some(vaddr);
+        }
+
+        // allocate a new 2MiB stack:
+        let alloc_result = PhysicalMemoryManager::alloc_huge_page();
+        if alloc_result.is_none() {
+            log::error!("Stack allocation failed, out of memory!");
+            return None;
+        }
+
+        let page = Page::from_address(VirtualAddress::from_u64(
+            proc_data.stack_space_start.as_u64()
+                + proc_data.n_stacks * 4 * MemorySizes::OneMib as u64,
+        ));
+
+        let map_result = vmm.map_huge_page(
+            page,
+            alloc_result.unwrap(),
+            PageEntryFlags::user_hugepage_flags(),
+        );
+
+        if map_result.is_err() {
+            log::error!(
+                "Stack allocation failed, error={:?}",
+                map_result.unwrap_err()
+            );
+            return None;
+        }
+
+        let vaddr = page.addr();
+        Self::zero(vaddr);
+
+        // increment the counter
+        proc_data.n_stacks += 2;
+        Some(vaddr)
+    }
+
+    #[inline]
+    pub fn free_stack(proc_data: &mut ProcessData, addr: VirtualAddress) {
+        let aligned_loc = Alignment::align_down(addr.as_u64(), 4 * MemorySizes::OneMib as u64);
+        let nth = aligned_loc / (4 * MemorySizes::OneMib as u64);
+
+        proc_data.free_stack_holes.push(nth);
+    }
 }
 
 pub fn map_user_stack(
