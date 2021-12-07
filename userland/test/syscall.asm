a6e1a9261005d04d0fa8c0abaafa2a4b4bc3cd2c
Narasimha Prasanna HN <yogaimpression@gmail.com>
2021-12-07 22:56:28 +0530

    Tested sys_sleepns

--- userland/test/syscall.asm
+++ userland/test/syscall.asm
@@ -2,6 +2,8 @@
 
 section .data
 msg: db "Hello, World!", 10
+t_sec: dq 4
+t_usec: dq 0
 
 global _start
 section .text
@@ -12,7 +14,8 @@ LOOP_START:
   mov rsi, msg
   mov rdx, 14
   int 0x80
-  mov rax, 42
+  mov rdi, t_sec
+  mov rax, 46
   int 0x80
   jmp LOOP_START
 
