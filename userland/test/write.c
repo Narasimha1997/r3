35d5158a95d90f1f0a52c920e72d26be924737f9
Narasimha1997 <yogaimpression@gmail.com>
2021-12-14 18:02:24 +0530

    The echo server

--- userland/test/write.c
+++ userland/test/write.c
@@ -10,15 +10,14 @@ i64 syscall(u64 rax, u64 rdi, u64 rsi, u64 rdx) {
         : "=a"(ret_val)
         : "0"(rax), "D"(rdi), "S"(rsi), "d"(rdx)
         : "rcx", "r11", "memory");
-
     return ret_val;
 }
 
 void _start() {
     char buffer[4096];
-    i64 read_length = 0, iter = 0;
     const char *welcome = "Welcome to ECHO program, I will echo whatever you say noob!.\n";
     const char *bullets = ">>>";
+    i64 read_length = 0, iter = 0;
     syscall(1, 1, (u64)welcome, 62);
     while (1) {
         syscall(1, 1, (u64)bullets, 6);
